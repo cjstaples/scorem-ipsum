4f8ea16c677422d9f1312394be96739e724e0dc2
Chuck Staples <cjstaples@gmail.com>
2025-02-27 17:03:46 -0500

    scoremipsem content for README.md; linting; version bump

--- scoremipsum/util/conversion.py
+++ scoremipsum/util/conversion.py
@@ -8,7 +8,8 @@ conversion
 conversion utils for the `scoremipsum` module.
 """
 import json
-import pprint
+# import pprint
+
 
 def convert_game_result_to_json(result_score, gametype=None):
     result_score_dict = []
@@ -26,7 +27,9 @@ def convert_game_result_to_json(result_score, gametype=None):
         result_score_dict.append(game_dict)
 
     result_score_json = json.dumps(result_score_dict, indent=4)
-    result_score_list = json.loads(result_score_json)
+
+    # for sanity checks
+    # result_score_list = json.loads(result_score_json)
     # print(f":::: conversion :: {result_score_list=}")
     # pprint.pprint(result_score_json)
 
